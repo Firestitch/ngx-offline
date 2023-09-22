8f17b3821951756dbcb75e9b6458f51f5b10728d
Raymond Gigliotti <ray@firestitch.com>
2023-09-22 11:57:51 -0400

    npm cleanup

--- src/app/offline/offline.module.ts
+++ src/app/offline/offline.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
-import { FsHtmlEditorModule } from '@firestitch/html-editor';
+import { FsHtmlRendererModule } from '@firestitch/html-editor';
 
 import { OfflineComponent } from './components';
 
@@ -13,7 +13,7 @@ import { OfflineComponent } from './components';
 
     MatCardModule,
 
-    FsHtmlEditorModule,
+    FsHtmlRendererModule,
   ],
   declarations: [
     OfflineComponent,
